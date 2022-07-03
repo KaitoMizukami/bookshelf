f41db50b9599070359e35849cfec1b39b41c3c0d
KaitoMizukami <kaito.program@gmail.com>
2022-07-03 15:18:31 +0900

    [change]Bookモデルを再定義

--- book/models.py
+++ book/models.py
@@ -2,3 +2,11 @@ from django.db import models
 
 # Create your models here.
 CATEGORY = (('business', 'ビジネス'), ('life', 'ライフ'), ('other', 'その他'))
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    category = models.CharField(
+        max_length=100,
+        choices=CATEGORY
+    )
