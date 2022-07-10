7d2df2978801fc6dcfcf3df57903845daa58ee7e
KaitoMizukami <kaito.program@gmail.com>
2022-07-11 00:01:12 +0900

    [add]リダイレクトの追記

--- book/views.py
+++ book/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
 
 from .models import Book
 
@@ -16,3 +17,4 @@ class CreateBookView(CreateView):
     template_name = 'book/book_create.html'
     model = Book
     fields = ('title', 'text', 'category')
+    success_url = reverse_lazy('list-book')
